3d4cda796fb45ea72680c6134e3d71f1c5f952dd
Suhas Bharadwaj <suhas.five@gmail.com>
2018-07-23 21:42:19 +0530

    Added launch request and unhandled intents and updated responses for errors.

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,9 @@ export class Handler {
         let client: rc.RestClient = new rc.RestClient("alexa-lambda", serverUri);
 
         let handlers: Alexa.Handlers = {
+            "LaunchRequest": function() {
+                this.emit(':ask', "Hello! Ask me to switch a light on, or off.");
+            },
             "AboutIntent": function() {
                 let self: Alexa.Handler = this;
                 let output: string = "This Alexa skill was developed by Suhas Bharadwaj";
@@ -20,15 +23,15 @@ export class Handler {
                 let intentRequest = <Alexa.IntentRequest> self.event.request;
                 let deviceType: string = intentRequest.intent.slots.DeviceType.value;
                 if (deviceType.toLowerCase() != "light") {
-                    this.emit(":tell", "This device is not supported yet!");
+                    this.emit(":ask", "This device is not supported yet!");
                 }
                 else {
                     client.get<DeviceState>("toggle").then(response => {
                         if (response.statusCode == 200 && response.result != null) {
-                            this.emit(":tell", `The ${deviceType} has been toggled ${response.result.isDeviceOn ? "on" : "off"}`);
+                            this.emit(":ask", `The ${deviceType} has been toggled ${response.result.isDeviceOn ? "on" : "off"}`);
                         }
                         else {
-                            this.emit(":tell", "There was an error while I tried to do that. Sorry about that!");
+                            this.emit(":ask", "There was an error while I tried to do that. Can you try again?");
                         }
                     });
                 }
@@ -40,24 +43,27 @@ export class Handler {
                 let deviceState: string = intentRequest.intent.slots.DeviceState.value;
 
                 if (deviceType.toLowerCase() != "light") {
-                    this.emit(":tell", "This device is not supported yet!");
+                    this.emit(":ask", "This device is not supported yet. Try something else!");
                 }
                 else {
                     var deviceRequest = new DeviceRequest({ requestedState: deviceState.toLowerCase()  });
                     client.create<DeviceState>("switch", deviceRequest).then(response => {
                         if(response.statusCode == 200 && response.result != null) {
                             if (!response.result.isStateUpdated) {
-                                this.emit(":tell", `The ${deviceType} is already switched ${deviceState}!`);
+                                this.emit(":ask", `The ${deviceType} is already switched ${deviceState}!`);
                             }
                             else {
-                                this.emit(":tell", `The ${deviceType} has been switched ${deviceState}`);
+                                this.emit(":ask", `The ${deviceType} has been switched ${deviceState}`);
                             }
                         }
                         else {
-                            this.emit(":tell", "There was an error while I tried to do that. Sorry about that!");
+                            this.emit(":ask", "There was an error while I tried to do that. Can you try again?");
                         }
                     });
                 }
+            },
+            "Unhandled": function(){
+                this.emit(':ask', 'I didn\'t quite get that. Come again, please?');
             }
         };
 
